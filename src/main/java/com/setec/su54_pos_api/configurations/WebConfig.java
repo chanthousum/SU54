5a9eb2c5198a80cbb8ad11bcd8689ab1916e460d
sum chanthou <chanthousum2017@gmail.com>
2026-01-04 10:18:08 +0700

    fix deployment --00014

--- src/main/java/com/setec/su54_pos_api/configurations/WebConfig.java
+++ src/main/java/com/setec/su54_pos_api/configurations/WebConfig.java
@@ -16,17 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler(new String[] { "/uploads/**" })
                 .addResourceLocations(new String[] { "file:" + this.uploadDir });
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
